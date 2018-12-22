f742165ffb01d26eef343d3c21f98f31c494041c
Skuznetz <s.kuznetzov@mail.ru>
2018-12-22 19:44:28 +0300

    напишем элемент

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+const el = (
+    <div>
+        <h1>Todo list</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+)
