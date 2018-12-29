b69dca8805c8baadb83063519fef09998874db27
Skuznetz <s.kuznetzov@mail.ru>
2018-12-29 12:03:31 +0300

    пишем теги TodoListItem

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,8 +4,8 @@ import TodoListItem from './todo-list-item'
 const TodoList = () => {
     return (
          <ul>
-            <li>1</li>
-            <li>2</li>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
         </ul>
     )
 }
