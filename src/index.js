babea60cdfa4f06cd2d3c4dcc79139de24d1fae3
Skuznetz <s.kuznetzov@mail.ru>
2018-12-29 11:43:00 +0300

    убираем TodoList

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const TodoList = () => {
-    return (
-         <ul>
-            <li>1</li>
-            <li>2</li>
-        </ul>
-    )
-}
+
 
 
 const App = () =>{
