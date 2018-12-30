934d5f467924ccb1aa6c7401987e1dbb2980c8cb
Skuznetz <s.kuznetzov@mail.ru>
2018-12-30 15:01:32 +0300

    напишем свойства

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,8 +4,8 @@ import TodoListItem from './todo-list-item'
 const TodoList = () => {
     return (
          <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            <li><TodoListItem label="Привет Иван" /></li>
+            <li><TodoListItem label="Привет Андрей" /></li>
         </ul>
     )
 }
