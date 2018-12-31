01dcef4b21c0b5838205d6eb68ff9515101a7bdc
Skuznetz <s.kuznetzov@mail.ru>
2018-12-31 11:04:45 +0300

    напишем массив

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import TodoList from './components/todo-list/todo-list.js'
 
 
 const App = () =>{
+    const todoData = [
+        {label: 'Просыпаемся',important: false},
+        {label: 'Завтракаем',important: false},
+        {label: 'уходим',important: false}        
+    ]
     return (
     <div>
         <AppHeader />
