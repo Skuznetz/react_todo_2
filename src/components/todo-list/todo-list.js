0f87b0f99e1ae748078eea8034401efb201d481d
Skuznetz <s.kuznetzov@mail.ru>
2019-01-02 13:27:42 +0300

    ставим key

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,7 +4,7 @@ import TodoListItem from '../todo-list-item/todo-list-item.js'
 const TodoList = ({todos}) => {
     const elements = todos.map((item) => {
         return (
-            <li> 
+            <li key={item.id}> 
                 <TodoListItem {...item} />
             </li>
         )
