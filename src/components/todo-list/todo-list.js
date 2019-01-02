c52e05f3bdba3ad1258fc913af63188c350331ec
Skuznetz <s.kuznetzov@mail.ru>
2019-01-02 13:13:05 +0300

    переберём массив

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 import TodoListItem from '../todo-list-item/todo-list-item.js'
 const TodoList = ({todos}) => {
+    const elements = todos.map((item) => {
+        return (
+            <li> 
+                <TodoListItem 
+                    label={item.label}
+                    important={item.important} />
+            </li>
+        )
+    })
     return (
          <ul>
-            <li><TodoListItem 
-            label="Привет Иван"
-            important /></li>
-            <li><TodoListItem label="Привет Андрей" /></li>
+            {elements}
         </ul>
     )
 }
