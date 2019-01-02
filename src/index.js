b080a15769bfb326ae06429168daa946d975fcad
Skuznetz <s.kuznetzov@mail.ru>
2019-01-02 13:25:52 +0300

    ставим id

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import TodoList from './components/todo-list/todo-list.js'
 
 const App = () =>{
     const todoData = [
-        {label: 'Просыпаемся',important: false},
-        {label: 'Завтракаем',important: false},
-        {label: 'уходим',important: false}        
+        {label: 'Просыпаемся',important: false,id: 1},
+        {label: 'Завтракаем',important: false,id:2},
+        {label: 'уходим',important: false,id:3}        
     ]
     return (
     <div>
