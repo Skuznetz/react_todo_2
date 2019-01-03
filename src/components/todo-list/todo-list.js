754bdc1cc5a6c9af1c30f1fb1d21de354a321e33
Skuznetz <s.kuznetzov@mail.ru>
2019-01-03 14:31:36 +0300

    пишем импорт

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import TodoListItem from '../todo-list-item/todo-list-item.js'
+import './todo-list.css'
 const TodoList = ({todos}) => {
     const elements = todos.map((item) => {
         const {id,...itemProps} = item
