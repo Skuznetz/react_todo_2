c24344d1c9376f05b833e6664b1b8de41a89af72
Skuznetz <s.kuznetzov@mail.ru>
2019-01-03 21:49:52 +0300

    создадим кнопки

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -6,9 +6,23 @@ const TodoListItem = ({label,important = false}) => {
     }
     return(
     <div 
-        className="todo-list-item" 
+        className="todo-list-item" >
+        <span
+        className="todo-list-item-label"
         style={style}>
         {label}
-    </div>)
+</span>)
+     
+      <button type="button"
+              className="btn btn -outline-success btn-sm float-right" 
+              onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+      <button type="button" 
+              className="btn btn-outline-danger btn-sm float-right"
+              onClick={onDeleted}>
+        <i className="fa fa-trash-o" />
+      </button>
+    </div>
 }
 export default TodoListItem
