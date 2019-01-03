94bd112713ef04531274fb94bde2a0b555e50d73
Skuznetz <s.kuznetzov@mail.ru>
2019-01-03 22:05:41 +0300

    уберём пока клики

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -15,13 +15,11 @@ const TodoListItem = ({label,important = false}) => {
 </span>)
      
       <button type="button"
-              className="btn btn -outline-success btn-sm float-right" 
-              onClick={onToggleImportant}>
+              className="btn btn -outline-success btn-sm float-right" >
         <i className="fa fa-exclamation" />
       </button>
       <button type="button" 
-              className="btn btn-outline-danger btn-sm float-right"
-              onClick={onDeleted}>
+              className="btn btn-outline-danger btn-sm float-right">
         <i className="fa fa-trash-o" />
       </button>
     </div>)
