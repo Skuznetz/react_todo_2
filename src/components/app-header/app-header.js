d68c2bbbdecdd833b8f326af052885af81edc755
Skuznetz <s.kuznetzov@mail.ru>
2019-01-04 18:06:46 +0300

    вставим аргументы

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,5 +1,9 @@
 import React from 'react'
-const AppHeader = () =>{
-    return <h1>Todo List</h1>
+const AppHeader = ({toDo,done}) =>{
+    return (
+        <div className="app-header d-flex">
+    <h1>Todo List</h1>
+    <h2>{toDo} ,{done}</h2>
+    </div>)
 }
 export default AppHeader
