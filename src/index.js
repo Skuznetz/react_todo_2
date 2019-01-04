dd96390167be33e485f507e8de9d4dcd0049af78
Skuznetz <s.kuznetzov@mail.ru>
2019-01-04 17:38:10 +0300

    уберём компонент App

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import AppHeader from './components/app-header/app-header.js'
-import Search from './components/search/search.js'
-import TodoList from './components/todo-list/todo-list.js'
-
-
-const App = () =>{
-    const todoData = [
-        {label: 'Просыпаемся',important: false,id: 1},
-        {label: 'Завтракаем',important: false,id:2},
-        {label: 'уходим',important: false,id:3}        
-    ]
-    return (
-    <div>
-        <AppHeader />
-        <Search />
-        <TodoList todos = {todoData} />
-</div>)
-}
+import App from './component/app'
 
 ReactDOM.render(<App />,document.getElementById('root'))
