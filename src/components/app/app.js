fa66e836805431031d74a87d6bfa638c845b8462
Skuznetz <s.kuznetzov@mail.ru>
2019-01-10 10:39:47 +0300

    соединяем массивы без удалённого элемента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,13 @@ export default class App extends Component {
     deliteItem=(id)=>{
         this.setState(({todoData})=>{
             const idx = todoData.findIndex((el)=>el.id ===id)
+            const newArray = [
+                ...todoData.slice(0,idx),
+                ...todoData.slice(idx+1)
+            ]
+            return {
+                todoData: newArray
+            }
         })
     }
     render(){
