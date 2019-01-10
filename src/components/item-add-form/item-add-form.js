e5dcddde00990f9563527127e936ea343e91e618
Skuznetz <s.kuznetzov@mail.ru>
2019-01-10 13:27:54 +0300

    создадим компонент

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,2 +1,16 @@
 import React,{Component} from 'react'
 import './item-add-form.css'
+
+export default class ItemAddForm extends Component{
+    render(){
+        return (
+            <div>
+                <input type="text"
+                       className="form-control"
+                       placeholder="What needs to be done"
+                 />
+                <button  className="btn btn-outline-secondary">Add item</button>
+            </div>
+        )
+    }
+}
