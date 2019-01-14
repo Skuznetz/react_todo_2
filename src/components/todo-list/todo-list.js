d843d96f1e88adee920f0ab5b95dd56b773f0415
Skuznetz <s.kuznetzov@mail.ru>
2019-01-14 11:10:45 +0300

    добавим аргументы

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import TodoListItem from '../todo-list-item/todo-list-item.js'
 import './todo-list.css'
-const TodoList = ({todos,onDeleted}) => {
+const TodoList = ({todos,onDeleted,onToggleImportant,onToggleDone}) => {
     const elements = todos.map((item) => {
         const {id,...itemProps} = item
         return (
