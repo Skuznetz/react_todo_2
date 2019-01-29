1ae3dd9a87ff03a27f15c9a431f16c0fe99d751c
Skuznetz <s.kuznetzov@mail.ru>
2019-01-29 13:55:50 +0300

    определим состояние

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import './search.css'
 class Search extends Component {
+    state = {term: ''}
     return(){
     return (
         <input placeholder="search" />
