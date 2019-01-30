b691f9f92cd08d280e3a102ee54f2b6eb6028905
Skuznetz <s.kuznetzov@mail.ru>
2019-01-30 14:05:38 +0300

    напишем функцию onSearchChange

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react'
 import './search.css'
 class Search extends Component {
     state = {term: ''}
+    onSearchChange=(e)=>{
+        const term=e.target.value
+        this.setState({term})
+        this.props.onSearchChange(term)
+    }
     return(){
     return (
         <input value={this.state.term} onChange={this.onSearchChange} placeholder="search" />
