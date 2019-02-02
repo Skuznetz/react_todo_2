efa70a5aec2590e1755606c27edaef7c2ff2386a
Skuznetz <s.kuznetzov@mail.ru>
2019-02-02 15:17:32 +0300

    выбираем кнопку

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -5,15 +5,24 @@ class ItemStatusFilter extends React.Component {
                {name: 'active',label: 'Active'},
                {name: 'done',label: 'Done'}]
     render(){
+
+        const buttons = this.buttons.map(({name,label})=>{
+        const isActive = filter === name
+        const clazz = isActive ? 'btn-info' : 'btn-outline-secondary'
+        return (
+            <button type="button" 
+                    className={`btn ${clazz}`}
+                    key={name}
+                    onClick={()=> onFilterChange(name)}>
+                {label}
+            </button>
+        )
+        
+    })
     return (
         <div className="btn-group">
-            <button type="button" 
-                    className="btn btn-info">All</button>
-            <button type="button" 
-                    className="btn btn-outline-secondary">Active</button>
-            <button type="button" 
-                    className="btn btn-outline-secondary">Done</button>
-        </div>
+            {buttons}
+        </div>   
     )
     }
 }
